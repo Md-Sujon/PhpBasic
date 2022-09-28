34fd3d062600058bd62d6fa2559d8cfc2531f27c
Md-Sujon <mdsujon829562@gmail.com>
2022-09-28 13:30:07 +0600

    php basic

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    /*
+   //variable 
+    $num = 50;
+    $num2 = 20;
+    $result = $num+$num2;
+    echo $num + $num2 ."<br>";
+$Age = 50;
+$Age1 = 40;
+$Age2 = 30;
+$Age3 = 20;
+echo $Age + $Age1."<br>" ;
+echo ($Age + $Age1)."<br>" ;
+ $result1 =  print $Age + $Age1."<br>" ;
+print ($Age + $Age1)."<br>";
+echo $result1;
+*/
+
+//Type
+
+$names = 'Sujon';
+$names2 = "I love you";
+$number = 30;
+$number2 = 50.5;
+$bol = true;
+$people = array('sujon', 'smita', 20.5, "kuk");
+var_dump($people);
+
+
+
+    
+    ?>
+</body>
+</html>
