e6741d793fa51b62b849714749205a4c782c3f62
Md-Sujon <mdsujon829562@gmail.com>
2022-09-29 19:14:10 +0600

    php basic

--- index.php
+++ index.php
@@ -26,6 +26,8 @@ print ($Age + $Age1)."<br>";
 echo $result1;
 */
 
+
+/*
 //Type
 
 $names = 'Sujon';
@@ -34,11 +36,111 @@ $number = 30;
 $number2 = 50.5;
 $bol = true;
 $people = array('sujon', 'smita', 20.5, "kuk");
-var_dump($people);
+$people1 = ['sujon', 'smita', 20.5, "kuk"];
+var_dump($people1);
+
+// echo $people; 
+
+*/
 
 
 
-    
+/*
+
+// object
+class phone  {
+    var $model;
+    function phoneModel ($number){
+        global $model;
+        $model = $number;
+        echo "This is phone $model <br>";
+    }
+}
+
+$apple = new phone;
+$apple -> phoneModel("apple 13");
+
+$vivo = new phone;
+$vivo -> phoneModel('Vivo y20')
+*/
+
+/*
+
+// String 
+ echo strlen('i love you');
+ echo strrev('i love you');
+ echo str_word_count('i love you');
+
+
+// echo strpos("i eat pizza", "eat");
+
+$food = str_replace("pizza","Rice","I eat pizza");
+echo($food);
+*/
+
+/*
+// number
+
+echo (max(66,20,90,50,30,20,21,65,78,15));
+echo (min(66,20,90,50,30,20,21,65,78,15));
+echo(round(pi()));
+
+echo(sqrt(68));
+echo(rand(1,30))
+  */
+  /*  
+//  constants 
+
+$x = 10;
+$y = 20;
+$z = 30;
+
+define('A', 30);
+define("B",30);
+define('cars', [
+    'BMW',
+    'toyato',
+    'tazla',
+    'honda'
+]);
+
+echo (cars[1]."<br>");
+  
+function host(){
+   echo (cars[1]); 
+}
+
+host();
+*/
+
+
+/*
+// condition
+
+$x = 20;
+$y = 20;
+
+if ($x===$y) {
+    echo 'same'.'<br>';
+}else echo 'not same'.'<br>';
+
+$date = date('H','M','Y'); 
+echo $date;
+*/
+/*
+// Loop
+
+$num = [54,4,5,8,2,3,9,4,1];
+for ($i=0; $i <= 100; $i+=5) { 
+   echo 'This is number '.$i."<br>";
+};
+*/
+
+// Time date
+date_default_timezone_set("Asia/Dhaka");
+echo "Today " .date('d/m/y') ."<br>";
+echo "Today " .date('h:m:sa');
+
     ?>
 </body>
 </html>
